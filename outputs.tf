a67aed9e2cb8668673e9bd4b6ba657b0f41a5523
Khue Doan <khuedoan98@gmail.com>
2021-11-11 20:55:35 +0700

    fix: change outputs after previous refactor

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "main_id" {
-  value = zerotier_network.main.id
+output "private_id" {
+  value = zerotier_network.private.id
+}
+
+output "public_id" {
+  value = zerotier_network.public.id
 }
