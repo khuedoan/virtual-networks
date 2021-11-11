10b5d24ce5b60ccd034e20a44aeb89f5639d7286
Khue Doan <khuedoan98@gmail.com>
2021-11-11 21:37:13 +0700

    style: Terraform format

--- members.tf
+++ members.tf
@@ -1,23 +1,23 @@
 resource "zerotier_member" "laptop" {
-  network_id  = zerotier_network.private.id
-  name        = "laptop"
-  member_id   = "4e464fc534"
+  network_id = zerotier_network.private.id
+  name       = "laptop"
+  member_id  = "4e464fc534"
 }
 
 resource "zerotier_member" "desktop" {
-  network_id  = zerotier_network.private.id
-  name        = "desktop"
-  member_id   = "5240e0cebe"
+  network_id = zerotier_network.private.id
+  name       = "desktop"
+  member_id  = "5240e0cebe"
 }
 
 resource "zerotier_member" "phone" {
-  network_id  = zerotier_network.private.id
-  name        = "phone"
-  member_id   = "2dca25d5e2"
+  network_id = zerotier_network.private.id
+  name       = "phone"
+  member_id  = "2dca25d5e2"
 }
 
 resource "zerotier_member" "macbook" {
-  network_id  = zerotier_network.private.id
-  name        = "macbook"
-  member_id   = "82312852d0"
+  network_id = zerotier_network.private.id
+  name       = "macbook"
+  member_id  = "82312852d0"
 }
