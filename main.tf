b0eebad26e88c3e2527e69c528112b1477724b7e
Khue Doan <khuedoan98@gmail.com>
2021-11-11 20:54:51 +0700

    refactor: create separate private and public networks

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-resource "zerotier_network" "main" {
-  name        = "main"
-  description = "Main network"
+resource "zerotier_network" "private" {
+  name        = "private"
+  description = "Private network"
   private     = true
+}
+
+resource "zerotier_network" "public" {
+  name        = "public"
+  description = "Public network"
+  private     = false
 }
