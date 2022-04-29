6f0ccc73d19c953087420aa8dfe6d0abaa3d90b0
Khue Doan <mail@khuedoan.com>
2022-04-29 23:20:24 +0700

    refactor(members): change desktop ID

--- members.tf
+++ members.tf
@@ -7,7 +7,7 @@ resource "zerotier_member" "laptop" {
 resource "zerotier_member" "desktop" {
   network_id = zerotier_network.private.id
   name       = "desktop"
-  member_id  = "d71e2ad8f9"
+  member_id  = "7221edfe12"
 }
 
 resource "zerotier_member" "phone" {
